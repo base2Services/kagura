1669e49eb849da21580eb4e5121cf8dee3e81c44
arran ubels <a.ubels@base2services.com>
2013-08-26 13:59:38 +1000

    Camel finally deploys.

---
+++ services/kagura-camel/src/main/java/com/base2/kagura/services/camel/Activator.java
@@ -0,0 +1,24 @@
+package com.base2.kagura.services.camel;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author aubels
+ *         Date: 26/08/13
+ */
+public class Activator implements BundleActivator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
+
+    public void start(BundleContext context) {
+        LOG.info("Starting the bundle");
+    }
+
+    public void stop(BundleContext context) {
+        LOG.info("Stopping the bundle");
+    }
+
+}
