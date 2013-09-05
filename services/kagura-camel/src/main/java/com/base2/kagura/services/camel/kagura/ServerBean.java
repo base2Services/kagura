ce223c79d152ac9276bc30819fc3c96f2605d1e6
arran ubels <a.ubels@base2services.com>
2013-09-05 11:51:01 +1000

    Application context now available in server bean.

--- services/kagura-camel/src/main/java/com/base2/kagura/services/camel/kagura/ServerBean.java
+++ services/kagura-camel/src/main/java/com/base2/kagura/services/camel/kagura/ServerBean.java
@@ -1,8 +1,10 @@
 package com.base2.kagura.services.camel.kagura;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
-import java.io.File;
-import java.net.URI;
+
 import java.net.URISyntaxException;
 import java.net.URL;
 
@@ -11,8 +13,9 @@ import java.net.URL;
  *         Date: 29/08/13
  */
 @Service()
-public class ServerBean {
+public class ServerBean implements ApplicationContextAware {
     private String configPath;
+    private ApplicationContext applicationContext;
 
     public ServerBean() {
         URL dir_url = ServerBean.class.getResource("/TestReports");
@@ -29,5 +32,30 @@ public class ServerBean {
 
     public void setConfigPath(String configPath) {
         this.configPath = configPath;
+    }
+
+    /**
+     * Set the ApplicationContext that this object runs in.
+     * Normally this call will be used to initialize the object.
+     * <p>Invoked after population of normal bean properties but before an init callback such
+     * as {@link org.springframework.beans.factory.InitializingBean#afterPropertiesSet()}
+     * or a custom init-method. Invoked after {@link org.springframework.context.ResourceLoaderAware#setResourceLoader},
+     * {@link org.springframework.context.ApplicationEventPublisherAware#setApplicationEventPublisher} and
+     * {@link org.springframework.context.MessageSourceAware}, if applicable.
+     *
+     * @param applicationContext the ApplicationContext object to be used by this object
+     * @throws org.springframework.context.ApplicationContextException
+     *          in case of context initialization errors
+     * @throws org.springframework.beans.BeansException
+     *          if thrown by application context methods
+     * @see org.springframework.beans.factory.BeanInitializationException
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
     }
 }
