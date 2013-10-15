6820bb29b24b3963c6d6a63aadf5760e3404c703
arran ubels <a.ubels@base2services.com>
2013-10-15 11:22:29 +1100

    SuperReportsFile mode for faster report loading.

---
+++ shared/reporting-core/src/main/java/com/base2/kagura/core/storage/SuperFileReports.java
@@ -0,0 +1,52 @@
+package com.base2.kagura.core.storage;
+
+import com.base2.kagura.core.report.configmodel.ReportsConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author aubels
+ *         Date: 15/10/13
+ */
+public class SuperFileReports extends AbstractReports<File> {
+    String superReportFile;
+
+    public SuperFileReports(String superReportFile) {
+        this.superReportFile = superReportFile;
+    }
+
+    public String getSuperReportFile() {
+        return superReportFile;
+    }
+
+    public void setSuperReportFile(String superReportFile) {
+        this.superReportFile = superReportFile;
+    }
+
+    @Override
+    protected String loadReport(ReportsConfig result, File report) throws Exception {
+        return null;
+    }
+
+    @Override
+    protected File[] getReportList() {
+        return null;
+    }
+
+    @Override
+    public ReportsConfig getReportsConfig() {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        ReportsConfig reportsConfig = null;
+        try {
+            reportsConfig = mapper.readValue(new File(superReportFile), ReportsConfig.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            errors.add("Error parsing super report " + superReportFile);
+        }
+        return reportsConfig;
+
+    }
+}
