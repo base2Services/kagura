848dd1bd839b99eb1df8204643abfe2df947dca0
arran ubels <a.ubels@base2services.com>
2013-12-13 12:26:14 +1100

    Fixed javascript path builder. War defaults to javascript assets.

--- example/javascript/src/main/java/com/base2/kagura/example/javascript/Utils.java
+++ example/javascript/src/main/java/com/base2/kagura/example/javascript/Utils.java
@@ -12,7 +12,10 @@ public class Utils {
     public static String serverPath(HttpServletRequest request)
     {
         try {
-            return new URL("http", request.getServerName(), request.getServerPort(), "/" + (request.getContextPath() != null ? request.getContextPath() : "")).toString();
+            String contextPath = request.getContextPath() != null ? request.getContextPath() : "";
+            if (!contextPath.endsWith("/") && contextPath.length() > 0)
+                contextPath += "/";
+            return new URL("http", request.getServerName(), request.getServerPort(),(!contextPath.startsWith("/")? "/":"") + contextPath).toString();
         } catch (MalformedURLException e) {
             e.printStackTrace();
             return "error";
