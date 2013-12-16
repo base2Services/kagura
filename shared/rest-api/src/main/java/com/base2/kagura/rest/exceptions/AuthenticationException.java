c3188cf2fadf93d583a5178e45ef67f5215fbf94
arran ubels <a.ubels@base2services.com>
2013-12-16 16:22:46 +1100

    Authentication exception was missing a throwable type.

--- shared/rest-api/src/main/java/com/base2/kagura/rest/exceptions/AuthenticationException.java
+++ shared/rest-api/src/main/java/com/base2/kagura/rest/exceptions/AuthenticationException.java
@@ -15,6 +15,17 @@ public class AuthenticationException extends WebApplicationException {
     }
 
     public AuthenticationException() {
-        super(Response.status(Response.Status.OK).entity(new ResponseBase() {{ setError("Authentication failure"); }} ).build());
+        super(Response.status(Response.Status.OK).entity(new ResponseBase() {{
+            setError("Authentication failure");
+        }}).build());
+    }
+
+    /**
+     * Construct a new instance with a blank message and default HTTP status code of 500.
+     *
+     * @param cause the underlying cause of the exception.
+     */
+    public AuthenticationException(Throwable cause) {
+        super(cause);
     }
 }
