cbbc24f7e7dfdb6d43035dca199fadef59c52da7
arran ubels <a.ubels@base2services.com>
2014-05-27 09:18:30 +1000

    Imported Ivy to allow for Grapes to work in GroovyShell.

---
+++ shared/reporting-core/src/test/java/com/base2/kagura/core/report/connectors/GroovyDataReportConnectorTest.java
@@ -0,0 +1,42 @@
+package com.base2.kagura.core.report.connectors;
+
+import com.base2.kagura.core.report.configmodel.GroovyReportConfig;
+import com.base2.kagura.core.report.configmodel.parts.ColumnDef;
+import com.base2.kagura.core.report.parameterTypes.ParamConfig;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * @author aubels
+ *         Date: 27/05/2014
+ */
+public class GroovyDataReportConnectorTest {
+    @Test
+    public void awsImportTest()
+    {
+        GroovyDataReportConnector groovyDataReportConnector = new GroovyDataReportConnector(new GroovyReportConfig()
+        {{
+                setColumns(new ArrayList<ColumnDef>());
+                setExtraOptions(new HashMap<String, String>());
+                setGroovy("@GrabResolver(name = 'mvnrepository', root = 'http://repo1.maven.org/maven2')\n" +
+                        "@Grapes(\n" +
+                        "        @Grab(group='com.amazonaws', module='aws-java-sdk', version='1.7.9')\n" +
+                        ")\n" +
+                        "import com.amazonaws.auth.AWSCredentials\n" +
+                        "import com.amazonaws.auth.BasicAWSCredentials\n" +
+                        "\n" +
+                        "AWSCredentials awsCredentials = new BasicAWSCredentials(\"\",\"\");\n" +
+                        "AmazonEC2Client amazonEC2Client = new AmazonEC2Client(awsCredentials);\n" +
+                        "\n" +
+                        "");
+                setParamConfig(new ArrayList<ParamConfig>());
+                setReportId("test");
+        }});
+        groovyDataReportConnector.run(new HashMap<String, Object>());
+        Assert.assertNotNull(groovyDataReportConnector.getRows());
+        Assert.assertTrue(groovyDataReportConnector.getRows().size() > 0);
+    }
+}
